c4c9507c42b0fe523c51b43986641ab6aa2522e4
lizhihao <2633002549@qq.com>
2020-05-17 23:39:36 +0800

    java动态绑定机制demo

---
+++ src/main/java/practice/lzh/javaTest/Sub.java
@@ -0,0 +1,34 @@
+package practice.lzh.javaTest;
+
+/**
+ * @author ：lizhihao
+ * @description：TODO
+ * @date ：2020/5/17
+ */
+public class Sub extends SupperT {
+
+    private String name = "sub";
+
+    public int age = 18;
+
+    @Override
+    public void supperTest() {
+        System.out.println("sub ...");
+    }
+
+//    @Override
+//    public void visitName() {
+//        System.out.println("name:" + this.name);
+//    }
+
+    public static void main(String[] args) {
+        SupperT sup = new Sub();
+        //由于动态绑定机制，所以调用子类重写的supperTest()方法，若子类没有重写supperTest()方法，那么会调用父类的supperTest()方法
+        //动态绑定机制只针对方法
+        sup.supperTest();
+        sup.visitName(); //若子类未重写visitName()，那么输出结果为：name:Supper
+
+        //属性不可以被重写，此时访问的还是父类的age
+        System.out.println(sup.age);
+    }
+}
---
+++ src/main/java/practice/lzh/javaTest/SupperT.java
@@ -0,0 +1,21 @@
+package practice.lzh.javaTest;
+
+/**
+ * @author ：lizhihao
+ * @description：TODO
+ * @date ：2020/5/17
+ */
+public class SupperT {
+
+    private String name = "Supper";
+
+    public int age = 42;
+
+    public void supperTest() {
+        System.out.println("SupperT ...");
+    }
+
+    public void visitName() {
+        System.out.println("name:" + this.name);
+    }
+}
