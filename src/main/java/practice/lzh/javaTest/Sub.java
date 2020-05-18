6e429b3e5d11d56b3767c75361ff193832a554b7
lizhihao <2633002549@qq.com>
2020-05-18 23:49:57 +0800

    add some notes

--- src/main/java/practice/lzh/javaTest/Sub.java
+++ src/main/java/practice/lzh/javaTest/Sub.java
@@ -25,6 +25,7 @@ public class Sub extends SupperT {
         SupperT sup = new Sub();
         //由于动态绑定机制，所以调用子类重写的supperTest()方法，若子类没有重写supperTest()方法，那么会调用父类的supperTest()方法
         //动态绑定机制只针对方法
+        //理解java的动态绑定机制，有助于理解scala中字段重写的原理
         sup.supperTest();
         sup.visitName(); //若子类未重写visitName()，那么输出结果为：name:Supper
 
